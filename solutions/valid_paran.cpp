c62971468001526edb3d2376a7d259c673e534ec
irving11119 <irving.deboer@gmail.com>
2023-05-05 11:41:56 +0800

    Update valid_paran solution

---
+++ solutions/valid_paran.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isValid(string s)
+    {
+        stack<char> st;
+
+        for (long i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if (st.empty())
+                {
+                    return false;
+                }
+
+                if (st.top() == '(' && s[i] != ')')
+                {
+                    return false;
+                }
+                else if (st.top() == '[' && s[i] != ']')
+                {
+                    return false;
+                }
+                else if (st.top() == '{' && s[i] != '}')
+                {
+                    return false;
+                }
+
+                st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
